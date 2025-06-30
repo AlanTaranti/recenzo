e1c7e4bac70886fdb4166c6e7b2aebae8be26568
Alan Taranti <contato@alantaranti.me>
2025-06-30 13:22:59 -0400

    Rename `createPrComment` to `createPrComments` in `IBitbucketService` and `ReviewPrUseCase` for consistency.

--- src/use-cases/review-pr-use-case.ts
+++ src/use-cases/review-pr-use-case.ts
@@ -10,7 +10,7 @@ interface IReviewerAgentService {
 
 interface IBitbucketService {
   getPrDiff: (workspace: string, repository: string, prNumber: number, ignoredFiles?: string[]) => Promise<string>;
-  createPrComment: (workspace: string, repository: string, prNumber: number, comments: BitBucketPrComment[]) => Promise<void>;
+  createPrComments: (workspace: string, repository: string, prNumber: number, comments: BitBucketPrComment[]) => Promise<void>;
 }
 
 type PrInfo = {
@@ -29,6 +29,6 @@ export class ReviewPrUseCase {
   public async reviewPr(prInfo: PrInfo): Promise<void> {
     const diff = await this.bitbucketService.getPrDiff(prInfo.workspace, prInfo.repository, prInfo.prNumber, prInfo.ignoredFiles);
     const codeReviewResult = await this.reviewerAgentService.review(diff);
-    await this.bitbucketService.createPrComment(prInfo.workspace, prInfo.repository, prInfo.prNumber, codeReviewResult);
+    await this.bitbucketService.createPrComments(prInfo.workspace, prInfo.repository, prInfo.prNumber, codeReviewResult);
   }
 }
