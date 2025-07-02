303e1c33455e1bd28fe832817315da1c559f18a6
Alan Taranti <contato@alantaranti.me>
2025-07-01 21:17:55 -0400

    Add `reviewPullRequest` method to `index.ts` integrating `ReviewPrUseCase` with services and repository

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { PullRequestInfo, ReviewPrOptions, ReviewPrUseCase } from '~/use-cases/review-pr-use-case';
+import { CodeReviewInstruction, ReviewerAgentService } from '~/services/reviewer-agent-service';
+import { BitbucketService } from '~/services/bitbucket-service';
+import { BitbucketRepository } from '~/repositories/bitbucket-repository';
+
+export default {
+  reviewPullRequest: (pullRequestInfo: PullRequestInfo, codeReviewInstruction: CodeReviewInstruction, options: ReviewPrOptions) => {
+    const useCase = new ReviewPrUseCase(new ReviewerAgentService(), new BitbucketService(new BitbucketRepository()));
+    return useCase.reviewPullRequest(pullRequestInfo, codeReviewInstruction, options);
+  },
+};
